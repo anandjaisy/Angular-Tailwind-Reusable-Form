1c3aa494e4fe0b732d44c9d7ce97982e088392af
anandjaisy <anandjaisy@gmail.com>
2023-01-19 21:40:53 +1100

    Auth call back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {Appearance, ControlType} from "../../projects/falcon-ng/tailwind/src/lib/model/enum";
+import {AuthService} from "../../projects/falcon-ng/tailwind/src/lib/service/open-id/auth.service";
+import {Button} from "../../projects/falcon-ng/tailwind/src/lib/control-type/Button";
 
 @Component({
   selector: 'app-root',
@@ -9,32 +11,28 @@ import {Appearance, ControlType} from "../../projects/falcon-ng/tailwind/src/lib
 export class AppComponent {
   title = 'Dynamic form builder';
   currentYear = new Date().getFullYear();
-  signInButtonConfig = {
-    componentProperty: {
-      label: 'Sign in',
-      appearance: Appearance.Raised,
-    },
-    componentType: ControlType.Button,
-    formControlName: 'Basic',
-  };
-  signOutButtonConfig = {
-    componentProperty: {
-      label: 'Sign out',
-      appearance: Appearance.Raised,
-    },
-    componentType: ControlType.Button,
-    formControlName: 'Basic',
-  };
-
+  signInButtonConfig = new Button({
+    label: 'Sign in',
+    appearance: Appearance.Raised,
+    color: 'primary',
+    class: 'flex justify-center'
+  });
+  signOutButtonConfig = new Button({
+    label: 'Sign out',
+    appearance: Appearance.Raised,
+    color: 'primary',
+    class: 'flex justify-center'
+  });
+  constructor(public authService: AuthService) {}
   SignInclick() {
-//    this.authService.isServiceReady().then(() => {
-//      this.authService.startAuthentication('/');
-//    });
+    this.authService.isServiceReady().then(() => {
+      this.authService.startAuthentication('/');
+    });
   }
 
   SignOutclick() {
-//    this.authService.isServiceReady().then(() => {
-//      this.authService.logout();
-//    });
+    this.authService.isServiceReady().then(() => {
+      this.authService.logout();
+    });
   }
 }
--- src/app/auth-callback/auth-callback.component.ts
+++ src/app/auth-callback/auth-callback.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {AppSettingService} from "../../../projects/falcon-ng/tailwind/src/lib/service/appsetting.service";
+import {AuthService} from "../../../projects/falcon-ng/tailwind/src/lib/service/open-id/auth.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {LoggerService} from "../../../projects/falcon-ng/tailwind/src/lib/service/logger.service";
 
 @Component({
   selector: 'app-auth-callback',
@@ -6,5 +10,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth-callback.component.scss']
 })
 export class AuthCallbackComponent {
+  private completed: boolean;
+  constructor(
+          private appSettings: AppSettingService,
+          private authService: AuthService,
+          private route: ActivatedRoute,
+          private router: Router,
+          private logger: LoggerService,
+          ) {}
 
+  async ngOnInit(): Promise<void> {
+    this.appSettings.on(AppSettingService.APP_SETTINGS_LOADED, () => {
+      this.completeAuthentication();
+    });
+    if (this.appSettings.initialized) {
+      await this.completeAuthentication();
+    }
+  }
+
+  async completeAuthentication() {
+    if (this.completed) {
+      this.logger.info(
+              '[AuthCallbackComponent] completeAuthentication',
+              'auth-callback: completed.',
+              );
+      return;
+    }
+    try {
+      const user = await this.authService.completeAuthentication();
+      const returnUrl = user.state
+        ? user.state.returnUrl
+        : this.route.snapshot.params['url'] || null;
+      this.completed = true;
+      this.logger.info(
+              '[AuthCallbackComponent] completeAuthentication',
+              'auth-callback: returnUrl completed.',
+              );
+      if (!returnUrl || returnUrl === '/auth-callback') {
+        this.router.navigateByUrl(location.origin);
+      } else if (returnUrl === '/') {
+        this.router.navigate(['']);
+      } else {
+        this.router.navigateByUrl(returnUrl);
+      }
+    } catch (e) {
+      this.authService.startAuthentication('/');
+    }
+  }
 }

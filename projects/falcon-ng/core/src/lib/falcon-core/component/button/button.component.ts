5f025efdd09eafcc84724cbb6691019eef116dad
anandjaisy <anandjaisy@gmail.com>
2025-12-28 11:10:48 +0545

    github files

--- projects/falcon-ng/core/src/lib/falcon-core/component/button/button.component.ts
+++ projects/falcon-ng/core/src/lib/falcon-core/component/button/button.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnDestroy,
-  Output,
-} from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
 
 @Component({
   selector: 'fal-button',
@@ -13,7 +7,9 @@ import {
       [matButton]="appearance"
       [type]="type"
       (click)="onClick($event)"
-      [disabled]="disabled">
+      [disabled]="disabled"
+      class="w-full"
+    >
       <span class="flex items-center justify-center gap-2 mdc-button__label">
         <ng-content></ng-content>
         <span class="mt-1">{{ label }}</span>
